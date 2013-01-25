1a95f0e0d31b454f7a7af690d5813116c40a3213
Matt Townsend <matt@broadcast.to>
2013-01-24 23:48:46 -0500

    Initial commit

---
+++ background.js
@@ -0,0 +1,106 @@
+// MIT Licensed
+// matt [at] nlts.co, 2012 
+
+// localstorage code from chromnibar extension source, MIT licensed, found at
+// http://code.google.com/p/chromnibar/
+
+// chrome.omnibox code modified from "omnix" omnibox example, which has the following copyright:
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+// http://developer.chrome.com/extensions/samples.html#be68e4d262d74d2457999fc402f5bf5e
+
+// sets the item in the localstorage
+var logging  = false;
+
+  function setItem(key, value) {
+      try {
+          log("Inside setItem:" + key + ":" + value);
+          window.localStorage.removeItem(key);
+          window.localStorage.setItem(key, value);
+      }catch(e) {
+          log("Error inside setItem");
+          log(e);
+      }
+      log("Return from setItem" + key + ":" +  value);
+  }
+  //Gets the item from local storage with the specified
+  //key...
+  function getItem(key) {
+      var value;
+      log('Get Item:' + key);
+      try {
+          value = window.localStorage.getItem(key);
+      }catch(e) {
+          log("Error inside getItem() for key:" + key);
+          log(e);
+          value = "null";
+      }
+      log("Returning value: " + value);
+      return value;
+  }
+  //Clears all the key value pairs in the local storage
+      function clearStrg() {
+      log('about to clear local storage');
+      window.localStorage.clear();
+      log('cleared');
+  }
+
+  function log(txt) {
+      if(logging) {
+          console.log(txt);
+      }
+  }
+
+  function removeItem(key) {
+      try {
+          log("Inside setItem:" + key + ":" + value);
+          window.localStorage.removeItem(key);
+      }catch(e) {
+          log("Error inside removeItem");
+          log(e);
+      }
+      log("Return from removeItem" + key + ":" +  value);
+  }
+
+
+chrome.omnibox.onInputChanged.addListener(
+  function(text, suggest) {
+    console.log('inputChanged: ' + text);
+  });
+
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    console.log('inputEntered: ' + text);
+    address = text.substr(0,text.indexOf(' '));
+    // set a custom address
+    if (address == "set") {
+      message = text.substr(text.indexOf(' ')+1);
+      key = message.substr(0,message.indexOf(' ')-1);
+      value = message.substr(message.indexOf(' ')+1);
+      setItem(key, value);
+    }
+      else{
+      if (getItem(address) !== null) {
+        address = getItem(address);
+      }
+      var subject = "";
+      var message = text.substr(text.indexOf(' ')+1);
+      // inputs subject if "sub .... ," exists in message
+      if (message.substr(0,3) === "sub") {
+        subject = message.substr(4,message.indexOf(',')-4);
+        message = message.substr(message.indexOf(',')+2);
+      }
+      if (subject !== "") {
+        subject = "?subject=" + subject;
+        message = "&body=" + message;
+      }
+      else
+      {
+        message = "?body=" + message;
+      }
+      chrome.windows.create({url: "mailto:"+address+subject+message, width: 760, height: 600});
+  }
+  });
